9dec6bcc0217472f295d002ce43a4478f006972a
Rob Selover <rselover@gmail.com>
2026-05-05 14:23:37 -0400

    Cleanup

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicnNlbG92ZXIiLCJhIjoiY21hbGJxMGxjMDZ6MDJtb3JqMWx5ZHh0YSJ9.50NQFxaZLsWruJ8dFIqfXw';
 
-const geojsonUrl = 'https://gist.githubusercontent.com/rselover/0ee2da020db757f2b35c8ec8b8b51888/raw/22a6f75fb1c6dca6073684e652dec25e27a98f83/Ski_Toddler.json';
-
+/*const geojsonUrl = 'https://gist.githubusercontent.com/rselover/0ee2da020db757f2b35c8ec8b8b51888/raw/22a6f75fb1c6dca6073684e652dec25e27a98f83/Ski_Toddler.json';
+*/
 function initMap() {
   const mapDiv = document.getElementById('map');
 
