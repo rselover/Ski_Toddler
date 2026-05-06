9cdc6d771c154b9c5c988ad281c6627a92ec5da0
Rob Selover <rselover@gmail.com>
2026-05-05 20:44:12 -0400

    Updated Data

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const geojsonUrl = 'https://gist.githubusercontent.com/rselover/0ee2da020db757f2b35c8ec8b8b51888/raw/48c8627512aa196fbef028f64ff42225e3ebdb0e/Ski_Toddler.json';
+const geojsonUrl = 'https://gist.githubusercontent.com/rselover/0ee2da020db757f2b35c8ec8b8b51888/raw/dafeacf9b2f088df015c44bf17f2c4ff143796b9/Ski_Toddler.json';
 
 function loadData() {
   return d3.json(geojsonUrl).then(data => 
